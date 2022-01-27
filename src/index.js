04f40a08b99123535002623fd1acc999637cd22c
helenheidemaa <98057078+helenheidemaa@users.noreply.github.com>
2022-01-27 22:32:19 +0200

    Updated the time

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
+//Calculate the date
+function formatDate(timestamp){
+let date = new Date (timestamp);
+let hours = date.getHours();
+ if (hours <10){
+          hours =`0${hours}`;
+}
+let minutes = date.getMinutes();
+
+ if (minutes <10){
+          minutes =`0${minutes}`;
+}
+
+let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+let day = days[date.getDay()]; 
+return `${day} ${hours}:${minutes}`;
+
+}
+
+
 function displayTemperature(response){
 console.log(response.data.name)
 let temperatureElement = document.querySelector("#temperature");
@@ -10,6 +30,8 @@ let humidityElement = document.querySelector("#humidity");
 humidityElement.innerHTML= response.data.main.humidity;
 let windspeedElement = document.querySelector("#wind-speed");
 windspeedElement.innerHTML= Math.round(response.data.wind.speed);
+let dateElement = document.querySelector("#date");
+dateElement.innerHTML= formatDate(response.data.dt * 1000);
 }
 
 
